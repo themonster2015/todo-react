931be129ad593671ff020fc89530cc7405ea2ffa
themonster2015 <quynhyen.vothi@gmail.com>
2021-09-20 16:58:16 +0200

    completed edit todo items

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -58,13 +58,25 @@ export default class TodoContainer extends Component {
                todos: [...this.state.todos, newTodo]
            })
        }
+
+       setUpdate = (updatedTitle, id) => {
+           this.setState({
+               todos: this.state.todos.map(todo => {
+                   if(todo.id === id){
+                       todo.title = updatedTitle
+                   }
+                   return todo
+               })
+           })
+      }
+
     render() {
         return (
             <div className="container"> 
             <div className="inner">
             <Header />
             <InputTodo addTodoProps={this.addTodoItem} />
-            <TodosList todos={this.state.todos} handleChangeProps={this.handleChange} deleteTodoProps={this.delTodo} />
+            <TodosList todos={this.state.todos} handleChangeProps={this.handleChange} deleteTodoProps={this.delTodo} setUpdate={this.setUpdate} />
             </div>
             </div>
         )
